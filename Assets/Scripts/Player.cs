40177872c1d053a1d7c68e0fedacd8e01ee4e3ff
EmersonicsDLSU <emerson_celestial@dlsu.edu.ph>
2022-05-20 08:39:10 +0800

    Added the Unity Server Model

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,91 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public int id;
+    public string username;
+    public CharacterController controller;
+    public float gravity = -9.81f;
+    public float moveSpeed = 5f;
+    public float jumpSpeed = 5f;
+
+    private bool[] inputs;
+    private float yVelocity = 0;
+
+    private void Start()
+    {
+        gravity *= Time.fixedDeltaTime * Time.fixedDeltaTime;
+        moveSpeed *= Time.fixedDeltaTime;
+        jumpSpeed *= Time.fixedDeltaTime;
+    }
+
+    public void Initialize(int _id, string _username)
+    {
+        id = _id;
+        username = _username;
+
+        inputs = new bool[5];
+    }
+
+    /// <summary>Processes player input and moves the player.</summary>
+    public void FixedUpdate()
+    {
+        Vector2 _inputDirection = Vector2.zero;
+        if (inputs[0])
+        {
+            _inputDirection.y += 1;
+        }
+        if (inputs[1])
+        {
+            _inputDirection.y -= 1;
+        }
+        if (inputs[2])
+        {
+            _inputDirection.x -= 1;
+        }
+        if (inputs[3])
+        {
+            _inputDirection.x += 1;
+        }
+
+        Move(_inputDirection);
+    }
+
+    /// <summary>Calculates the player's desired movement direction and moves him.</summary>
+    /// <param name="_inputDirection"></param>
+    private void Move(Vector2 _inputDirection)
+    {
+        Vector3 _moveDirection = transform.right * _inputDirection.x + transform.forward * _inputDirection.y;
+        _moveDirection *= moveSpeed;
+        // if the player is on the ground
+        if (controller.isGrounded)
+        {
+            yVelocity = 0f;
+            // if jump key is pressed
+            if (inputs[4])
+            {
+                yVelocity = jumpSpeed;
+            }
+        }
+        // constantly add gravity to our player
+        yVelocity += gravity;
+        // update our y position
+        _moveDirection.y = yVelocity;
+        // updates our new transform to the CharacterController
+        controller.Move(_moveDirection);
+
+        ServerSend.PlayerPosition(this);
+        ServerSend.PlayerRotation(this);
+    }
+
+    /// <summary>Updates the player input with newly received input.</summary>
+    /// <param name="_inputs">The new key inputs.</param>
+    /// <param name="_rotation">The new rotation.</param>
+    public void SetInput(bool[] _inputs, Quaternion _rotation)
+    {
+        inputs = _inputs;
+        transform.rotation = _rotation;
+    }
+}
